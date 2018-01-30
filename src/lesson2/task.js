f9d483c148e6dfd0bd7b3b011c55bb94d13bf6a0
Maxim <maxim.kondzera@gmail.com>
2018-01-30 12:59:42 +0200

    lesson2 refactoring

--- src/lesson2/task.js
+++ src/lesson2/task.js
@@ -2,15 +2,13 @@
   Напишите функцию, которая параметрами принимает 2 числа и возвращает их сумму
 */
 
-export const sum = (a, b) => Number(a) + Number(b);
+export const sum = (a, b) => +a + +b;
 
 /*
   Напишите функцию, которая возвращает сумму всех чисел, что передаются параметрами
 */
 
-export const sumAll = (...args) => args.reduce((acc, curr) => {
-  return acc + curr;
-}, 0);
+export const sumAll = (...args) => args.reduce((acc, curr) => acc + +curr, 0);
 /*
   Напишите функцию, которая возвращает число x в степень n
 */
@@ -20,7 +18,8 @@ export const pow = (x, n) => x ** n;
   Напишите функцию, которая возвращает рандомное целое число от from до to
 */
 export const random = (from, to) => (
-  Math.floor(Math.random() * (to - from)) + from
+  /* eslint no-mixed-operators: ["error", {"allowSamePrecedence": true}] */
+  Math.floor(Math.random() * (to - from + 1)) + from
 );
 
 export default {
