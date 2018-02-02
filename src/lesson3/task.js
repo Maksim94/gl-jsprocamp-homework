b7771ad51f8bbc197e996ec3cd00482831bfbf9b
Maxim <maxim.kondzera@gmail.com>
2018-02-02 11:25:32 +0200

    lesson3: addLogCapability refactoring, rewrite bindContext, logger, compose and partial

--- src/lesson3/task.js
+++ src/lesson3/task.js
@@ -3,15 +3,22 @@
   Function should return a number - amount of optional parameters that were passed into function.
   Hint: you are allowed to modify both function definition and function body.
 */
+/* eslint func-names: ["error", "never"] */
+
 export const countOptional = (first, second, ...args) => args.length;
 
 /*
   Write your implementation of native Function.prototype.bind method
 */
-export const bindContext = (fn, context, ...outerArgs) => (...args) => (
-  fn.apply(context, [...outerArgs, ...args])
-);
+// export const bindContext = (fn, context, ...outerArgs) => (...args) => (
+//   fn.apply(context, [...outerArgs, ...args])
+// );
 
+export function bindContext(fn, context, ...outerArgs) {
+  return function (...args) {
+    return fn.apply(context, [...outerArgs, ...args]);
+  };
+}
 
 /*
   Write function that accepts 1 parameter - object. It should add to this object a log interface so as:
@@ -27,35 +34,47 @@ export const bindContext = (fn, context, ...outerArgs) => (...args) => (
 
   Take to account, that you should track log call index starting from 1
 */
-const getLoggerMessage = object => (
-  object.name
-    ? `Log message #${object.i}: my name is ${object.name}`
-    : `Log message #${object.i}: I dont have name`
-);
-export const addLogCapability = object => {
-  object.i = 0;
-  object.log = function log() {
-    object.i += 1;
-    return getLoggerMessage(object);
+export function addLogCapability(object) {
+  let counter = 0;
+
+  object.log = function () {
+    counter += 1;
+
+    const logMessage = `Log message #${counter}`;
+    const nameMessage = object.name ? `my name is ${object.name}` : 'I dont have name';
+
+    return `${logMessage}: ${nameMessage}`;
   };
-};
+}
 
 /*
   Write a function that creates custom topic logger:
   myLogger = logger('My Topic')
   myLogger('first message'); //=> My Topic: first message
 */
-export const logger = topic => message => `${topic}: ${message}`;
+// export const logger = topic => message => `${topic}: ${message}`;
+
+export function logger(topic) {
+  return function (message) {
+    return `${topic}: ${message}`;
+  };
+}
 
 /*
   Implement left to right compose function
 */
 
-// Ectualy it's pipe, not compose
+// Actually it's pipe, not compose
 /* Compose looks like (from right to left):
   const compose = (...fns) => x => fns.reduceRight((f, g) => g(f), x);
 */
-export const compose = (...fns) => x => fns.reduce((f, g) => g(f), x);
+// export const compose = (...fns) => x => fns.reduce((f, g) => g(f), x);
+
+export function compose(...fns) {
+  return function (initial) {
+    return fns.reduce((f, g) => g(f), initial);
+  };
+}
 
 /*
   Implement function that can turn function into partial application
@@ -67,7 +86,13 @@ export const compose = (...fns) => x => fns.reduce((f, g) => g(f), x);
   const sumWith4 = partialSum(4);
   sumWith4(5) // 9
 */
-export const partial = fn => (...args) => fn.bind(null, ...args);
+// export const partial = fn => (...args) => fn.bind(null, ...args);
+
+export function partial(fn) {
+  return function (...args) {
+    return fn.bind(null, ...args);
+  };
+}
 
 export default {
   countOptional,
